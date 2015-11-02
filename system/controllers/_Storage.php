53d1cc84e3473fdbaf4a2c7dd6446b79b878446f
Koen Everaert <koen@coff.ie>
2015-11-02 16:31:17 +0100

    Added $storage-unit to save objects to the session, say, a user, and re-use it during the entire session.

---
+++ system/controllers/_Storage.php
@@ -0,0 +1,63 @@
+<?php
+
+	Class Storage {
+		var $storage;
+
+		function __construct(){
+			//Controller initialized
+			if(!isset($_SESSION['_storage'])){
+				$this->storage = array();
+				$_SESSION['_storage'] = serialize($this->storage);
+			}else{
+				$this->storage = unserialize($_SESSION['_storage']);
+			}
+		}
+
+		function __destruct(){
+			//Save container
+			$session_storage = unserialize($_SESSION['_storage']);
+			foreach($this->storage as $key => $var){
+				$test = false;
+				if(is_object($var) || is_array($var)){
+					foreach($var as $key_object => $value_object){
+						if($value_object !== null){
+							$test = true;
+						}
+					}
+				}elseif($var !== null){
+					$test = true;
+				}
+				if($test){
+					$session_storage[$key] = $var;
+				}
+			}
+			$_SESSION['_storage'] = serialize($session_storage);
+		}
+
+		function __get($key){
+			if(is_array($this->storage) && array_key_exists($key, $this->storage)){
+				return $this->storage[$key];
+			}else{	
+				$obj = new $key();
+				$this->storage[$key] = $obj;
+				return $this->storage[$key];
+			}
+		}
+
+		function index(){
+	    	$this->error->trigger(404, "Page does not exist");
+		}
+
+		public function add($name, $object){
+			$this->storage[$name] = $object;
+		}
+
+		public function get($name){
+			if(array_key_exists($name, $this->storage)){
+				return $this->storage[$name];
+			}else{
+				return false;
+			}
+		}
+	}
+?>
